4b9f533043bd3e21e75dfd5389aee0baa2c1903a
nathanmaru <55788297+nathanmaru@users.noreply.github.com>
2021-06-20 18:01:39 +0800

    YourVisits Module Prep

---
+++ ShareLockRevamp/Adapters/YourVisitsAdapter.cs
@@ -0,0 +1,70 @@
+﻿using Android.Support.V7.Widget;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+
+namespace ShareLockRevamp.Adapters
+{
+    class YourVisitsAdapter : RecyclerView.Adapter
+    {
+        public event EventHandler<YourVisitsAdapterClickEventArgs> ItemClick;
+        public event EventHandler<YourVisitsAdapterClickEventArgs> ItemLongClick;
+        List<Request> items;
+
+        public YourVisitsAdapter(string[] data)
+        {
+            items = data;
+        }
+
+        // Create new views (invoked by the layout manager)
+        public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
+        {
+
+            //Setup your layout here
+            View itemView = null;
+            //var id = Resource.Layout.__YOUR_ITEM_HERE;
+            //itemView = LayoutInflater.From(parent.Context).
+            //       Inflate(id, parent, false);
+
+            var vh = new YourVisitsAdapterViewHolder(itemView, OnClick, OnLongClick);
+            return vh;
+        }
+
+        // Replace the contents of a view (invoked by the layout manager)
+        public override void OnBindViewHolder(RecyclerView.ViewHolder viewHolder, int position)
+        {
+            var item = items[position];
+
+            // Replace the contents of the view with that element
+            var holder = viewHolder as YourVisitsAdapterViewHolder;
+            //holder.TextView.Text = items[position];
+        }
+
+        public override int ItemCount => items.Length;
+
+        void OnClick(YourVisitsAdapterClickEventArgs args) => ItemClick?.Invoke(this, args);
+        void OnLongClick(YourVisitsAdapterClickEventArgs args) => ItemLongClick?.Invoke(this, args);
+
+    }
+
+    public class YourVisitsAdapterViewHolder : RecyclerView.ViewHolder
+    {
+        //public TextView TextView { get; set; }
+
+
+        public YourVisitsAdapterViewHolder(View itemView, Action<YourVisitsAdapterClickEventArgs> clickListener,
+                            Action<YourVisitsAdapterClickEventArgs> longClickListener) : base(itemView)
+        {
+            //TextView = v;
+            itemView.Click += (sender, e) => clickListener(new YourVisitsAdapterClickEventArgs { View = itemView, Position = AdapterPosition });
+            itemView.LongClick += (sender, e) => longClickListener(new YourVisitsAdapterClickEventArgs { View = itemView, Position = AdapterPosition });
+        }
+    }
+
+    public class YourVisitsAdapterClickEventArgs : EventArgs
+    {
+        public View View { get; set; }
+        public int Position { get; set; }
+    }
+}
---
+++ ShareLockRevamp/EventListeners/RequestListener.cs
@@ -0,0 +1,60 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Firebase.Database;
+using System;
+using ShareLockRevamp.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ShareLockRevamp.Helpers;
+
+namespace ShareLockRevamp.EventListeners
+{
+    public class RequestListener : Java.Lang.Object, IValueEventListener
+    {
+        List<Request> requestList = new List<Request>();
+        public event EventHandler<RequestDataEventArgs> RequestRetrived;
+
+        public class RequestDataEventArgs : EventArgs
+        {
+            public List<Request> Request { get; set; }
+        }
+        public void OnCancelled(DatabaseError error)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnDataChange(DataSnapshot snapshot)
+        {
+            if (snapshot.Value != null)
+            {
+                var child = snapshot.Children.ToEnumerable<DataSnapshot>();
+                requestList.Clear();
+                foreach (DataSnapshot memberData in child)
+                {
+                    Request request = new Request();
+                    request.ID = memberData.Key;
+                    request.DoorLockId = memberData.Child("DoorLockID").Value.ToString();
+                    request.Fullname = memberData.Child("Fullname").Value.ToString();
+                    request.Message = memberData.Child("Message").Value.ToString();
+                    request.OwnerUsername = memberData.Child("OwnerUsername").Value.ToString();
+                    request.isApprove = memberData.Child("isApprove").Value.ToString();
+                    request.UserID = memberData.Child("UserID").Value.ToString();
+                    request.OneTimePassword = memberData.Child("OTP").Value.ToString();
+
+                    requestList.Add(request);
+                }
+                RequestRetrived.Invoke(this, new RequestDataEventArgs { Request = requestList });
+            }
+        }
+        public void Create()
+        {
+            DatabaseReference requestRef = AppDataHelper.GetDatabase().GetReference("requestInfo");
+            requestRef.AddValueEventListener(this);
+        }
+    }
+}
